65f74ef5a3231c7c28b4bd678ca0cf9673b337dd
Julian Dev <jemahechaj@ut.edu.co>
2019-04-28 16:48:24 -0500

    Alert Page

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  inicio: string ='Alert';
+
+  constructor(public alertCtlr: AlertController) {  }
 
   ngOnInit() {
+    
+  }
+  /* Alerta Normal */
+  async presentAlert() {
+    const alert = await this.alertCtlr.create({
+      header: 'Alerta',
+      subHeader: 'Subtitle',
+      message: 'Esto es un mensaje de alerta.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Boton Cancelar');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Boton Ok');
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 
+  /* Recibiendo información por medio de Prompt */
+  async presentPrompt() {
+    const alert = await this.alertCtlr.create({
+      header: 'Cambia el Titulo',
+      subHeader: 'Ingresa un titulo',
+      message: ':)',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Boton Cancelar');
+          }
+        }, {
+          text: 'Okay',
+          handler: data => {
+            this.inicio = data.titulo;
+            console.log(data);
+          }
+        },
+      ],
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Cambia el Titulo'
+      }
+    ]
+    });
+    
+    await alert.present();
+  }
 }
